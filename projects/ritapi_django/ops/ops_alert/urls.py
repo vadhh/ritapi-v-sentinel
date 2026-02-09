9fa934c702afd5126adde2af0c9ce74f31754868
vadhh <attridho@gmail.com>
2026-02-09 15:11:24 +0700

    Fix URL pattern for alert resolution by removing leading slash

--- projects/ritapi_django/ops/ops_alert/urls.py
+++ projects/ritapi_django/ops/ops_alert/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     # ALERTS
     path("", views.alert_dashboard, name="ops.ops_alert"),
-    path("/resolve/<int:alert_id>/", views.resolve_alert, name="ops_resolve_alert"),
+    path("resolve/<int:alert_id>/", views.resolve_alert, name="ops_resolve_alert"),
     path("alert_chart_data/", views.alert_chart_data, name="alert_chart_data"),
 
 ]
